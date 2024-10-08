ad9cb02df9586bec78a853d891063916bed14767
Rasmus Ring <rasri696@student.liu.se>
2024-10-08 19:31:51 +0200

    struktureade upp koden och la till lite och sånt

--- TDDI41/generate_accounts.py
+++ TDDI41/generate_accounts.py
@@ -1,31 +1,47 @@
 import random
-
-file = open('/courses/TDDI41/names-tricky', 'r', encoding='utf-8', errors='ignore')
-
-names = file.readlines()
-
-file.close()
-
-accountnames = []
+import subprocess
+import sys
+import string
 
 
-for name in names:
+def generate_username(name):
     if " " in name:
         splitname = name.split(' ', 2)
         tmpname = splitname[0].lower()[:2] + splitname[1].lower()[:2] + str(random.randint(100, 999))
     else:
         splitname = name
         tmpname = splitname.lower()[:4] + str(random.randint(100, 999))
+    return tmpname
+
+
+def create_user(username):
+    subprocess.run(["useradd", "-m", username])
+
+
+def set_password(username, password):
+    subprocess.run()
     
-    ##här skulle vi kunna kontrollera för dubletter
-    #om dublett ta bort sista siffror och lägg till nytt random nummer
-    accountnames.append(tmpname)
+
+def generate_password():
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for i in range(10))
 
 
-print("hello world")
+def main(filename):
+    file = open(filename, 'r', encoding='utf-8', errors='ignore')
+    names = file.readlines()
+    file.close()
 
-print(names)
+    for name in names:
+        username = generate_username(name)
+        password = generate_password()
+        create_user(username)
+        set_password(username, password)
+        print(f"User {username} created with password {password}")
 
-print(accountnames)
 
-
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: generate_accounts.py ")
+        sys.exit(1)
+    main(sys.argv[1])
